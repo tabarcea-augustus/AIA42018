2fbbfc54ab31fc8a634e928ffbe3cc64181929d6
NinjaSaph <andra.oica@yahoo.com>
2018-11-30 02:23:04 +0200

    Smol indentation changes

--- Mapping/main.py
+++ Mapping/main.py
@@ -4,67 +4,72 @@
 #Dunno if these DB functions will remain here
 
 def checkDB(word):
-    #Could return true or false and translation
-    #return True,"ok"
-    return False,""
+	#Could return true or false and translation
+	#return True,"ok"
+	return False, ""
     
 def checkDex(word):
-    #Could return true or false and translation
-    #return True,"ok"
-    return True,"ok"
+	#Could return true or false and translation
+	#return True,"ok"
+	return True,"ok"
    
 def setMeaning(word, meaning):
-    #Could update DB with DEX found translation
-    pass
+	#Could update DB with DEX found translation
+	pass
 
 ################################################
 
 def getArhaicList():
-    #Get the list from somewhere...
-    #Work in progress...
-    global arhaicList
-    arhaicList = ["mere","pere"]
+	#Get the list from somewhere...
+	#Work in progress...
+	global arhaicList
+	arhaicList = ["mere","pere"]
 
 def translate():
-    #TRANSLATE function
-    #Updates global final list with translated words
-    global arhaicList, finalList
-    #Parse through list and seek words' meaning
-    for word in arhaicList:
-        #print(word)
-	#Check for word in DB
-        translation = checkDB(word)
-	#If found...
-        if(translation[0]):
-	    #Append translation
-            finalList.append(translation[1])
-        else:
-	    #Otherwise check online on DEX
-            translation = checkDex(word)
-	    #If found...
-            if(translation[0]):
-		#Append translation
-                finalList.append(translation[1])
-		#Update DB with found word and translation
-                setMeaning(word,translation[1])
-            else:
-		#Otherwise append nothing
-                finalList.append("?")
+	#TRANSLATE function
+	#Updates global final list with translated words
+	global arhaicList, finalList
+	#Parse through list and seek words' meaning
+	for word in arhaicList:
+		#print(word)
+		#Check for word in DB
+		translation = checkDB(word)
+		#If found...
+		if(translation[0]):
+			#Append translation
+			finalList.append(translation[1])
+		else:
+			#Otherwise check online on DEX
+			translation = checkDex(word)
+			#If found...
+			if(translation[0]):
+				#Append translation
+				finalList.append(translation[1])
+				#Update DB with found word and translation
+				setMeaning(word,translation[1])
+			else:
+				#Otherwise append nothing
+				finalList.append("?")
 
-################## MAIN ####################
+################## MAIN #####################
+
 def main():
-    global arhaicList, finalList
-    #Get the arhaic terms list
-    getArhaicList()
-    #Update final list with translated words
-    translate()
-    print(arhaicList)
-    print(finalList)
+	global arhaicList, finalList
+	#Get the arhaic terms list
+	getArhaicList()
+	#Update final list with translated words
+	translate()
+	print(arhaicList)
+	print(finalList)
 
 #############################################
+
 #Initial list containing arhaic words
 arhaicList = []
 #Final list of translated words
 finalList = []
+
 #############################################
+
 main()
+
