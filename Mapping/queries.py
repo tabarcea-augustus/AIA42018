5254820e43b0468179f22b7c415a56ba618fc02f
TeodorDom <claw_teodor@yahoo.com>
2019-01-05 01:45:42 +0200

    Added code for checking the offline db.

--- Mapping/queries.py
+++ Mapping/queries.py
@@ -4,7 +4,13 @@ def getTranslation(word, cursor):
     cursor.execute("SELECT translation FROM DICT where word = ?", (word,))
     translation = cursor.fetchone()
     if translation is None:
-        return False, (None,)
+        cursor.execute("SELECT inflection_dia FROM lexem where inflection_dia = ?", (word,))
+        translation = cursor.fetchone()
+        if translation is None:
+            cursor.execute("SELECT inflection FROM lexem where inflection = ?", (word,))
+            translation = cursor.fetchone()
+            if translation is None:
+                return False, (None,)
     return True, translation
 
 ##dupa insert-uri, delete-rui, update-uri trebuie sa dai commit (din cate am inteles)
@@ -13,6 +19,8 @@ def updateTranslation(oldword, newword, newt, cursor, conn):
     conn.commit()
 
 def insertWord(word, translation, cursor, conn):
+    if translation == False:
+        translation = word
     cursor.execute("""INSERT INTO DICT VALUES (? , ?)""", (word, translation))
     conn.commit()
 
